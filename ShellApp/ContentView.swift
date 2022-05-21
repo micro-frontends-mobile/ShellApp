2ceaae84a818bb7add8535d3015be382a35846b9
zdd <zddhub@gmail.com>
2022-05-21 15:52:45 +0800

    Initial commit

---
+++ ShellApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShellApp
+//
+//  Created by Dongdong Zhang on 2022/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShellApp/ShellAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShellAppApp.swift
+//  ShellApp
+//
+//  Created by Dongdong Zhang on 2022/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ShellAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
