6b81c36e93300c7c20207241bced78a07eb3e62f
zdd <zddhub@gmail.com>
2022-05-21 15:55:55 +0800

    Add tab view

--- ShellApp/ContentView.swift
+++ ShellApp/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    TabView {
+      Text("Restaurant")
+        .tabItem {
+          Image(systemName: "1.square.fill")
+          Text("Restaurant")
+        }
+      Text("Surprise me")
+        .tabItem {
+          Image(systemName: "2.square.fill")
+          Text("Surprise me")
+        }
+      Text("About")
+        .tabItem {
+          Image(systemName: "3.square.fill")
+          Text("About")
+        }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
